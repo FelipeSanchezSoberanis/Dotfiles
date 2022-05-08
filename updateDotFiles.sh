870afd19aa94ae3ebe9753115c58276428c5d505
Felipe  Sánchez <fs@wayakna.com>
2022-05-08 15:30:11 -0500

    Ranger init configs

--- updateDotFiles.sh
+++ updateDotFiles.sh
@@ -6,3 +6,9 @@ cp ~/.gitmessage .
 cp ~/.gitconfig .
 cp ~/.config/i3/config .
 cp ~/.config/kitty/kitty.conf .
+
+cp ~/.config/ranger/rifle.conf .
+cp ~/.config/ranger/commands.py .
+cp ~/.config/ranger/commands_full.py .
+cp ~/.config/ranger/rc.conf .
+cp ~/.config/ranger/scope.sh .
