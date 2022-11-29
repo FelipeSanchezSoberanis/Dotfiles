fe3eb7f8b43d2880a117b6e8278fc7ed356e0787
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-11-29 14:12:57 -0600

    Add lock png to lock screen

--- scripts/lock-screen.sh
+++ scripts/lock-screen.sh
@@ -2,7 +2,7 @@
 
 gnome-screenshot -f /tmp/screenshot.png
 convert /tmp/screenshot.png -filter Gaussian -blur 0x16 /tmp/screenshot.png
-# convert bg.png \( test.png -scale 50% \) -gravity southeast  -composite output.png;
+convert /tmp/screenshot.png \( /home/felipe/Pictures/assets/lock.png -scale 10% \) -gravity center -composite /tmp/screenshot.png;
 i3lock -i /tmp/screenshot.png -t
 rm /tmp/screenshot.png
 
