5ccace3b9199c3b881aa3b8e2abe0051337e1e8e
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-02-22 11:56:55 -0600

    Changed rofi theme

--- scripts/zathura-open.sh
+++ scripts/zathura-open.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-file=$(fdfind --base-directory / -e pdf -I -a | rofi -dmenu)
+file=$(fdfind --base-directory / -e pdf -I -a | rofi -dmenu -theme ~/.config/rofi/launchers/type-4/style-2.rasi)
 
 # Exit if $file is empty
 if test -z "$file"
