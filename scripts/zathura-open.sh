97ad9b26c4448ff85b253f3243e7ce84714d9f98
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-02-28 15:18:10 -0600

    Test if file is valid and allow whitespaces

--- scripts/zathura-open.sh
+++ scripts/zathura-open.sh
@@ -2,11 +2,11 @@
 
 file=$(fdfind --base-directory /home/felipe/ -e pdf -I | rofi -dmenu -theme ~/.config/rofi/launchers/type-4/style-2.rasi)
 
-# Exit if $file is empty
-if test -z "$file"
+# Exit if $file is empty or not a valid file
+if [ -z "$file" ] || [ ! -f "$file" ]
 then
     exit
 fi
 
-nohup zathura /home/felipe/$file > /dev/null
+nohup zathura "/home/felipe/$file" > /dev/null
 
