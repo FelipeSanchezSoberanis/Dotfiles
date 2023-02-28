1b6badef75f48008cd02c0bce3b97ec0c0301ca7
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-02-28 17:07:04 -0600

    Rofi case insensitive

--- scripts/zathura-open.sh
+++ scripts/zathura-open.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-file=$(fdfind --base-directory /home/felipe/ -e pdf -I | rofi -dmenu -theme ~/.config/rofi/launchers/type-4/style-2.rasi)
+file=$(fdfind --base-directory /home/felipe/ -e pdf -I | rofi -dmenu -i -theme ~/.config/rofi/launchers/type-4/style-2.rasi)
 
 # Exit if $file is empty or not a valid file
 if [ -z "$file" ] || [ ! -f "$file" ]
