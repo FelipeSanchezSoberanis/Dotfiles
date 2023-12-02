6a326283482738a7f99591d1fbe2f3ab31926312
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-12-01 19:58:41 -0600

    Change git message line length and kitty opacity

--- nvim/indentBlankLinesConfig.lua
+++ nvim/indentBlankLinesConfig.lua
@@ -1,7 +1,3 @@
 vim.opt.list = true
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true
-}
+require("ibl").setup {}
