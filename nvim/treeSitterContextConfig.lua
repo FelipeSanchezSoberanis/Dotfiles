a1faf14cbb6deaefef63f16b5722a7d206127c83
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-02-06 15:26:37 -0600

    Formatting

--- nvim/treeSitterContextConfig.lua
+++ nvim/treeSitterContextConfig.lua
@@ -1,14 +1,14 @@
-require'treesitter-context'.setup {
+require"treesitter-context".setup {
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
         -- For all filetypes
         -- Note that setting an entry here replaces all other patterns for this entry.
         -- By setting the 'default' entry below, you can control which nodes you want to
         -- appear in the context window.
         default = {
-            'class', 'function', 'method'
+            "class", "function", "method"
             -- 'for', -- These won't appear in the context
             -- 'while',
             -- 'if',
@@ -30,6 +30,6 @@ require'treesitter-context'.setup {
     --     you can safely ignore them.
 
     zindex = 20, -- The Z-index of the context window
-    mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-    separator = '-' -- Separator between context and content. Should be a single character string, like '-'.
+    mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+    separator = "-" -- Separator between context and content. Should be a single character string, like '-'.
 }
