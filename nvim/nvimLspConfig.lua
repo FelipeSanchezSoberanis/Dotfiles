2c61df9f355f89b2e1c64f8951bfe8fb2e38c174
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-02-05 23:52:24 -0600

    Formatting

--- nvim/nvimLspConfig.lua
+++ nvim/nvimLspConfig.lua
@@ -1,8 +1,8 @@
-local NODE_MODULES = os.getenv("NODE_HOME") .. '/lib/node_modules'
+local NODE_MODULES = os.getenv("NODE_HOME") .. "/lib/node_modules"
 local DEBOUNCE_TIME = 500;
 
 -- Setup nvim-cmp.
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 cmp.setup({
     completion = {autocomplete = false},
@@ -20,38 +20,38 @@ cmp.setup({
         end
     },
     mapping = {
-        ['<C-j>'] = cmp.mapping.select_next_item({
+        ["<C-j>"] = cmp.mapping.select_next_item({
             behavior = cmp.SelectBehavior.Select
         }),
-        ['<C-k>'] = cmp.mapping.select_prev_item({
+        ["<C-k>"] = cmp.mapping.select_prev_item({
             behavior = cmp.SelectBehavior.Select
         }),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
         })
     },
     sources = cmp.config.sources({
-        {name = 'nvim_lsp'}, -- { name = 'vsnip' }, -- For vsnip users.
+        {name = "nvim_lsp"}, -- { name = 'vsnip' }, -- For vsnip users.
         -- { name = 'luasnip' }, -- For luasnip users.
-        {name = 'ultisnips'} -- For ultisnips users.
+        {name = "ultisnips"} -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
-    }, {{name = 'buffer'}})
+    }, {{name = "buffer"}})
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
+cmp.setup.cmdline("/", {sources = {{name = "buffer"}}})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
+cmp.setup.cmdline(":", {
+    sources = cmp.config.sources({{name = "path"}}, {{name = "cmdline"}})
 })
 
-local nvim_lsp = require('lspconfig')
+local nvim_lsp = require("lspconfig")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -64,36 +64,36 @@ local on_attach = function(client, bufnr)
     end
 
     -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
     local opts = {noremap = true, silent = true}
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+    buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>",
                    opts)
-    buf_set_keymap('n', '<space>wa',
-                   '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr',
-                   '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl',
-                   '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+    buf_set_keymap("n", "<space>wa",
+                   "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wr",
+                   "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    buf_set_keymap("n", "<space>wl",
+                   "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
                    opts)
-    buf_set_keymap('n', '<space>D',
-                   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
+    buf_set_keymap("n", "<space>D",
+                   "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>",
                    opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>',
+    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>",
                    opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>',
+    buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+    buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+    buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>",
                    opts)
     -- buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>',
     --                opts)
@@ -101,25 +101,25 @@ local on_attach = function(client, bufnr)
 end
 
 -- Show borders around floating windows
-require('lspconfig.ui.windows').default_options.border = 'single'
+require("lspconfig.ui.windows").default_options.border = "single"
 
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
     opts = opts or {}
-    opts.border = 'single'
+    opts.border = "single"
     return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = {
-    'pyright', 'lua_ls', 'cssls', 'html', 'jsonls', 'bashls', 'dockerls',
-    'lemminx', 'eslint', 'texlab', 'arduino_language_server', 'rust_analyzer',
-    'clangd', 'phpactor', 'kotlin_language_server', 'angularls', 'emmet_ls',
-    'yamlls', 'groovyls', 'tsserver'
+    "pyright", "lua_ls", "cssls", "html", "jsonls", "bashls", "dockerls",
+    "lemminx", "eslint", "texlab", "arduino_language_server", "rust_analyzer",
+    "clangd", "phpactor", "kotlin_language_server", "angularls", "emmet_ls",
+    "yamlls", "groovyls", "tsserver"
 }
 for _, lsp in ipairs(servers) do
-    if (lsp == 'html' or lsp == 'cssls' or lsp == 'jsonls' or lsp == 'eslint') then
+    if (lsp == "html" or lsp == "cssls" or lsp == "jsonls" or lsp == "eslint") then
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities.textDocument.completion.completionItem.snippetSupport =
             true
@@ -130,10 +130,10 @@ for _, lsp in ipairs(servers) do
             capabilities = capabilities,
             workspace = {didChangeWatchedFiles = {dynamicRegistration = true}}
         }
-    elseif (lsp == 'angularls') then
+    elseif (lsp == "angularls") then
         local cmd = {
-            'ngserver', '--stdio', '--tsProbeLocations', NODE_MODULES,
-            '--ngProbeLocations', NODE_MODULES
+            "ngserver", "--stdio", "--tsProbeLocations", NODE_MODULES,
+            "--ngProbeLocations", NODE_MODULES
         }
 
         nvim_lsp[lsp].setup {
@@ -143,7 +143,7 @@ for _, lsp in ipairs(servers) do
             on_new_config = function(new_config) new_config.cmd = cmd end,
             workspace = {didChangeWatchedFiles = {dynamicRegistration = true}}
         }
-    elseif (lsp == 'lua_ls') then
+    elseif (lsp == "lua_ls") then
         nvim_lsp[lsp].setup {
             on_attach = on_attach,
             flags = {debounce_text_changes = DEBOUNCE_TIME},
@@ -151,11 +151,11 @@ for _, lsp in ipairs(servers) do
                 Lua = {
                     runtime = {
                         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                        version = 'LuaJIT'
+                        version = "LuaJIT"
                     },
                     diagnostics = {
                         -- Get the language server to recognize the `vim` global
-                        globals = {'vim'}
+                        globals = {"vim"}
                     },
                     workspace = {
                         -- Make the server aware of Neovim runtime files
@@ -167,25 +167,25 @@ for _, lsp in ipairs(servers) do
             },
             workspace = {didChangeWatchedFiles = {dynamicRegistration = true}}
         }
-    elseif (lsp == 'groovyls') then
+    elseif (lsp == "groovyls") then
         nvim_lsp[lsp].setup {
             cmd = {
-                'java', '-jar',
-                '/home/felipe/Documents/groovy-language-server/build/libs/groovy-language-server-all.jar'
+                "java", "-jar",
+                "/home/felipe/Documents/groovy-language-server/build/libs/groovy-language-server-all.jar"
             },
             on_attach = on_attach,
             flags = {debounce_text_changes = DEBOUNCE_TIME},
             workspace = {didChangeWatchedFiles = {dynamicRegistration = true}}
         }
-    elseif (lsp == 'tsserver') then
+    elseif (lsp == "tsserver") then
         nvim_lsp[lsp].setup {
             on_attach = on_attach,
             flags = {debounce_text_changes = DEBOUNCE_TIME},
             workspace = {didChangeWatchedFiles = {dynamicRegistration = true}},
             init_options = {
-                host_info = 'neovim',
+                host_info = "neovim",
                 tsserver = {
-                    path = NODE_MODULES .. '/typescript/lib/tsserver.js'
+                    path = NODE_MODULES .. "/typescript/lib/tsserver.js"
                 }
             }
         }
