cf507baddfe320974f9d9fe44525e00c1c1063bc
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-08-05 10:08:41 -0600

    refactor: libinput configs from script to file

--- scripts/set-natural-scroll.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-touchpad_id=$(xinput list | rg -i "touchpad" | sed "s/.\+id=\([0-9]\+\)\+.\+/\1/g")
-
-natural_scroll_property=$(xinput list-props $touchpad_id | rg -i "natural scroll" | sed "s/.\+default.\+//gi")
-natural_scroll_property_id=$(echo $natural_scroll_property | sed "s/.\+(\([0-9]\+\)).\+/\1/g")
-
-xinput set-prop $touchpad_id $natural_scroll_property_id 1
-
