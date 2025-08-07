214ec138ecf88530a92711a6d100d8d058a60d72
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2025-08-07 08:21:27 -0600

    feat: simpler rgflow flags

--- nvim/lua/plugin-configs/rgflow.lua
+++ nvim/lua/plugin-configs/rgflow.lua
@@ -1,5 +1,5 @@
 require("rgflow").setup({
-    cmd_flags = "--smart-case --fixed-strings --ignore --max-columns 200",
+    cmd_flags = "-S -.",
     default_trigger_mappings = true,
     default_ui_mappings = true,
     default_quickfix_mappings = true
