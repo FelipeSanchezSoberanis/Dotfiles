c6dd930b0061255c6b18cb9e1919a242f543d4eb
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2025-10-21 18:44:22 -0600

    feat: update jdtls

--- nvim/lua/plugin-configs/nvim-jdtls.lua
+++ nvim/lua/plugin-configs/nvim-jdtls.lua
@@ -9,10 +9,10 @@ vim.api.nvim_create_autocmd("FileType", {
                     "-Dlog.level=ALL", "-Xmx1g", "--add-modules=ALL-SYSTEM", "--add-opens",
                     "java.base/java.util=ALL-UNNAMED", "--add-opens",
                     "java.base/java.lang=ALL-UNNAMED",
-                    "-javaagent:/home/felipe/Documents/executables/lombok/lombok.jar", "-jar",
-                    "/home/felipe/Documents/executables/jdt-language-server-1.38.0/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar",
+                    "-javaagent:/home/felipe/Documents/lombok/lombok.jar", "-jar",
+                    "/home/felipe/Documents/jdt-language-server-1.51.0/plugins/org.eclipse.equinox.launcher_1.7.0.v20250519-0528.jar",
                     "-configuration",
-                    "/home/felipe/Documents/executables/jdt-language-server-1.38.0/config_linux",
+                    "/home/felipe/Documents/jdt-language-server-1.51.0/config_linux",
                     "-data", "/home/felipe/.jdtls-data/" .. vim.fn.getcwd():gsub("%/", "__")
                 },
                 root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"}),
