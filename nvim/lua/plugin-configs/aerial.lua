2c7b98602cc21e54d5d3af18433394bb63bf8fa1
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2026-03-20 07:33:03 -0600

    feat: update aerial config

--- nvim/lua/plugin-configs/aerial.lua
+++ nvim/lua/plugin-configs/aerial.lua
@@ -1,1 +1,6 @@
-require("aerial").setup({backends = {json = {"lsp", "treesitter"}}})
+require("aerial").setup({
+    backends = {"lsp", "treesitter"},
+    layout = {default_direction = "prefer_left"},
+    disable_max_lines = 100000,
+    disable_max_size = 2000000
+})
