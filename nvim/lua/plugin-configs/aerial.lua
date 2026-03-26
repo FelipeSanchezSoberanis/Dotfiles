c42e5c3e0857857120690f7b094d63365331a0da
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2026-03-26 13:36:51 -0600

    feat: enable folding, update aerial symbols

--- nvim/lua/plugin-configs/aerial.lua
+++ nvim/lua/plugin-configs/aerial.lua
@@ -1,6 +1,10 @@
 require("aerial").setup({
     backends = {"lsp", "treesitter"},
-    layout = {default_direction = "prefer_left"},
+    layout = {default_direction = "prefer_left", placement = "edge"},
     disable_max_lines = 100000,
-    disable_max_size = 2000000
+    disable_max_size = 2000000,
+    filter_kind = {
+        "Class", "Constructor", "Enum", "Function", "Interface", "Module", "Method", "Struct",
+        "Array"
+    }
 })
