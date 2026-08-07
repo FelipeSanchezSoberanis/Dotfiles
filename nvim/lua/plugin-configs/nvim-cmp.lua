64fc55c02e41278c73f74fbfbaaced1ab99060b0
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2026-08-07 08:30:31 -0600

    feat: nvim-cmp custom sorting

--- nvim/lua/plugin-configs/nvim-cmp.lua
+++ nvim/lua/plugin-configs/nvim-cmp.lua
@@ -1,4 +1,19 @@
 local cmp = require("cmp")
+local compare = cmp.config.compare
+local Kind = cmp.lsp.CompletionItemKind
+
+local function fields_properties_methods(entry_1, entry_2)
+    local function should_prioritize_entry(entry)
+        local kind = entry:get_kind()
+        return kind == Kind.Field or kind == Kind.Property or kind == Kind.Method
+    end
+
+    local prioritize_entry_1 = should_prioritize_entry(entry_1)
+    local prioritize_entry_2 = should_prioritize_entry(entry_2)
+
+    if prioritize_entry_1 ~= prioritize_entry_2 then return prioritize_entry_1 end
+    return nil
+end
 
 cmp.setup({
     snippet = {expand = function(args) vim.snippet.expand(args.body) end},
@@ -15,7 +30,15 @@ cmp.setup({
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4)
     }),
-    sources = cmp.config.sources({{name = "nvim_lsp"}}, {{name = "buffer"}})
+    sources = cmp.config.sources({{name = "nvim_lsp"}}, {{name = "buffer"}}),
+    sorting = {
+        priority_weight = 2,
+        comparators = {
+            fields_properties_methods, compare.offset, compare.exact, compare.scopes, compare.score,
+            compare.recently_used, compare.locality, compare.kind, compare.sort_text,
+            compare.length, compare.order
+        }
+    }
 })
 
 cmp.setup.cmdline({"/", "?"},
